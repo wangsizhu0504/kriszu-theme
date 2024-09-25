ca65f68e25276c39cd9cf02eadf8070c92794e2d
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-09-25 09:53:47 +0800

    feat: add extra theme

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import process from 'node:process'
 import fs from 'fs-extra'
 import getTheme from './theme'
+import { getXtermTheme } from './extra'
 
 fs.mkdir('./themes', { recursive: true })
   .then(() => Promise.all([
@@ -35,6 +36,36 @@ fs.mkdir('./themes', { recursive: true })
         color: 'dark',
         name: 'Kriszu Dark Soft',
         soft: true,
+      }),
+      { spaces: 2 },
+    ),
+  ]))
+  .catch(() => process.exit(1))
+
+fs.mkdir('./extra', { recursive: true })
+  .then(() => Promise.all([
+    fs.writeJSON(
+      './extra/xterm-vitesse-light.json',
+      getXtermTheme({
+        color: 'light',
+        name: 'Vitesse Light',
+      }),
+      { spaces: 2 },
+    ),
+    fs.writeJSON(
+      './extra/xterm-vitesse-dark.json',
+      getXtermTheme({
+        color: 'dark',
+        name: 'Vitesse Dark',
+      }),
+      { spaces: 2 },
+    ),
+    fs.writeJSON(
+      './extra/xterm-vitesse-black.json',
+      getXtermTheme({
+        color: 'dark',
+        name: 'Vitesse Black',
+        black: true,
       }),
       { spaces: 2 },
     ),
